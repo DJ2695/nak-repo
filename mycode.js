526d07d9033ed9a8509ef7c269f2910c597e2e0d
TimCrush <tgk1702@googlemail.com>
2020-11-07 16:28:34 +0100

    conflict master

--- mycode.js
+++ mycode.js
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
+DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD<<<<<<< HEAD
 1st change master
 =======
 1st change in master mycode.js
 >>>>>>> feature123
 1 line in javascript
+
